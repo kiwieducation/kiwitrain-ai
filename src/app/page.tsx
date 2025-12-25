38ee66d9b0632d724253bee37b799b1f8c18c599
kiwieducation <0katherinewang@gmail.com>
2025-12-25 17:10:43 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,2 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  redirect("/dashboard");
-}
+import Dashboard from "@/components/Dashboard";
+export default function Page(){ return <Dashboard/> }
